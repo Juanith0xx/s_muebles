ecda9cb6502992f15b46b7c4af76dc308da27fd0
Juan Estay Rodriguez <juan.estay.rodriguez@icloud.com>
2025-05-28 10:15:15 -0400

    Optimizacion SEO v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from 'react-helmet-async'; // 👈 Import necesario
 import './App.css';
+
 import Nosotros from './components/Nosotros';
 import Navbar from './components/Navbar';
 import ClientesGrid from './components/ClientesGrid';
@@ -9,13 +11,11 @@ import Home from './components/Home';
 import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
 import ServicePanel from './components/ServicePanel';
-
 import OficinasCorporativa from './pages/OficinasCorporativa';
 import MueblesClinicosHospitalarios from './pages/MueblesClinicosHospitalarios';
 import Puertas from './pages/Puertas';
 import RevestimientoFenolico from './pages/RevestimientoFenolico';
 import { MarcosTelescopicos } from './pages/MarcosTelescopicos';
-
 
 function App() {
   const [darkMode, setDarkMode] = useState(() => {
@@ -33,42 +33,41 @@ function App() {
   }, [darkMode]);
 
   return (
-    <Router>
-      {/* Botón opcional para cambiar tema */}
-      <Navbar />
+    <HelmetProvider>
+      <Router>
+        {/* Botón opcional para cambiar tema */}
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={
-          <>
-            <div id="home">
-              <Home />
-            </div>
-            <div id="nosotros">
-              <Nosotros />
-            </div>
-            <div id="nuestros_clientes">
-              <ClientesGrid />
-            </div>
-            <div id="nuestra_fabrica">
-              <FactoryVideo />
-            </div>
-            <div id="contacto">
-              <ContactForm />
-            </div>
-            <Footer />
-          </>
-        } />
-        <Route path="/servicios" element={<ServicePanel />} />
-        <Route path="/proyectos/oficinas-corporativa" element={<OficinasCorporativa />} />
-        <Route path="/proyectos/muebles_clinicos_hospitalarios" element={<MueblesClinicosHospitalarios />} />
-        <Route path="/proyectos/puertas" element={<Puertas />} />
-        <Route path="/proyectos/revestimiento_fenolico" element={<RevestimientoFenolico />} />
-        <Route path="/proyectos/marcos_telescopicos" element={<MarcosTelescopicos/>} />
-        
-
-        
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <div id="home">
+                <Home />
+              </div>
+              <div id="nosotros">
+                <Nosotros />
+              </div>
+              <div id="nuestros_clientes">
+                <ClientesGrid />
+              </div>
+              <div id="nuestra_fabrica">
+                <FactoryVideo />
+              </div>
+              <div id="contacto">
+                <ContactForm />
+              </div>
+              <Footer />
+            </>
+          } />
+          <Route path="/servicios" element={<ServicePanel />} />
+          <Route path="/proyectos/oficinas-corporativa" element={<OficinasCorporativa />} />
+          <Route path="/proyectos/muebles_clinicos_hospitalarios" element={<MueblesClinicosHospitalarios />} />
+          <Route path="/proyectos/puertas" element={<Puertas />} />
+          <Route path="/proyectos/revestimiento_fenolico" element={<RevestimientoFenolico />} />
+          <Route path="/proyectos/marcos_telescopicos" element={<MarcosTelescopicos />} />
+        </Routes>
+      </Router>
+    </HelmetProvider>
   );
 }
 
